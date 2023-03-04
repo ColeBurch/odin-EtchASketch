0066314360a36b131ed1537eb837ae0855848137
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-03-04 15:26:11 -0700

    Add mouseover listener to page to turn elements black

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const etchASketch = document.querySelector(".etchASketch")
 
 for (let i = 0; i < 256; i++) {
-    const cube = document.createElement('div')
-    cube.classList.add('cube')
-    cube.style.cssText = 'height: 58px; width: 58px; background-color: white; margin: 1px;'
-    etchASketch.appendChild(cube)
+    const createCube = document.createElement('div')
+    createCube.classList.add('cube')
+    createCube.style.cssText = 'height: 58px; width: 58px; margin: 1px;'
+    createCube.classList.add('white')
+    etchASketch.appendChild(createCube)
 }
+
+const cube = document.querySelectorAll(".cube")
+console.log(cube)
+
+cube.forEach((div) => {
+    div.addEventListener('mouseover', () => {
+        div.classList.remove('white')
+        div.classList.add('colored')
+    })
+})
