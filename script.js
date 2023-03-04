3ab5ec6b66ab1d5681d71a032c89d903bfc2b135
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-03-04 09:38:43 -0700

    Create single cube element with js

---
+++ script.js
@@ -0,0 +1,5 @@
+const etchASketch = document.querySelector(".etchASketch")
+
+const cube = document.createElement('div')
+cube.style.cssText = 'height: 60px; width: 60px'
+etchASketch.appendChild(cube)
