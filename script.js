ef2f88b54aa20d4085086b476fbe249d296828e5
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-03-04 10:19:29 -0700

    Create cube outline to define grid

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const etchASketch = document.querySelector(".etchASketch")
 for (let i = 0; i < 256; i++) {
     const cube = document.createElement('div')
     cube.classList.add('cube')
-    cube.style.cssText = 'height: 60px; width: 60px'
+    cube.style.cssText = 'height: 58px; width: 58px; background-color: white; margin: 1px;'
     etchASketch.appendChild(cube)
 }
