19d1fd6814f34a085a9d311bf836b096d1726c7e
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-03-04 09:46:18 -0700

    Add loop to create grid

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const etchASketch = document.querySelector(".etchASketch")
 
-const cube = document.createElement('div')
-cube.style.cssText = 'height: 60px; width: 60px'
-etchASketch.appendChild(cube)
+for (let i = 0; i < 256; i++) {
+    const cube = document.createElement('div')
+    cube.classList.add('cube')
+    cube.style.cssText = 'height: 60px; width: 60px'
+    etchASketch.appendChild(cube)
+}
