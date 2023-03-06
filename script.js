4ebe14f04d2130e6669b33c3c5ad54ceeb43b2b1
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-03-06 15:38:16 -0700

    Add change grid function and link it to change grid button

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 const etchASketch = document.querySelector(".etchASketch")
 const reset = document.querySelector(".reset")
+const changeBoard = document.querySelector(".changeBoard")
 
 reset.addEventListener('click', () => {
+    let cube = document.querySelectorAll(".cube")
     cube.forEach((div) => {
         div.classList.remove('colored')
         div.classList.add('white')
     })
 })
 
+changeBoard.addEventListener('click', () => {
+    changeGrid()
+})
+
 for (let i = 0; i < 256; i++) {
-    const createCube = document.createElement('div')
+    let createCube = document.createElement('div')
     createCube.classList.add('cube')
     createCube.style.cssText = 'height: 58px; width: 58px; margin: 1px;'
     createCube.classList.add('white')
     etchASketch.appendChild(createCube)
 }
 
-const cube = document.querySelectorAll(".cube")
+let cube = document.querySelectorAll(".cube")
 console.log(cube)
 
 cube.forEach((div) => {
@@ -25,3 +31,39 @@ cube.forEach((div) => {
         div.classList.add('colored')
     })
 })
+
+function changeGrid() {
+    let num = prompt("How many elements in each row?")
+    if (num > 0 && num < 100) {
+        let size = (960 / num) - 2
+        let elementNumber = num ** 2
+        console.log(size)
+        console.log(elementNumber)
+        let cube = document.querySelectorAll(".cube")
+        cube.forEach((div) => {
+            div.remove()
+        })
+        for (let i = 0; i < elementNumber; i++) {
+            let createCube = document.createElement('div')
+            createCube.classList.add('cube')
+            createCube.style.cssText = 'margin: 1px;'
+            createCube.style.height = size + "px"
+            createCube.style.width = size + "px"
+            createCube.classList.add('white')
+            etchASketch.appendChild(createCube)
+            console.log(createCube)
+        }
+    } else {
+        alert("Invalid Input")
+        changeGrid()
+    }
+    let cube = document.querySelectorAll(".cube")
+    console.log(cube)
+
+    cube.forEach((div) => {
+        div.addEventListener('mouseover', () => {
+            div.classList.remove('white')
+            div.classList.add('colored')
+    })
+})
+}
